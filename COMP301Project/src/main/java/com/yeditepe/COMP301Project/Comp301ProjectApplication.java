bd11758ce90faa7073ee968224e1a66dad499f10
bbf304 <bbf304@yeditepe.idr>
2025-12-22 11:16:33 +0300

    Initial commit

---
+++ COMP301Project/src/main/java/com/yeditepe/COMP301Project/Comp301ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yeditepe.COMP301Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp301ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp301ProjectApplication.class, args);
+	}
+
+}
---
+++ COMP301Project/src/test/java/com/yeditepe/COMP301Project/Comp301ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeditepe.COMP301Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp301ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
